cae24355ea2c6955fa1b659f031227eb50372e95
ongaro09 <ongaromark7@gmail.com>
2024-05-06 03:48:41 +0300

    check the status of the API and update the UI

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    checkAPIStatus();
+
+    function checkAPIStatus() {
+        $.get("http://0.0.0.0:5001/api/v1/status", function(data) {
+            if (data.status === "OK") {
+                $('#api_status').addClass('available');
+            } else {
+                $('#api_status').removeClass('available');
+            }
+        });
+    }
+});
